2814e7713df954708a2be94a755ff3da87f39562
roberto <rd.navaro@gmail.com>
2024-08-26 22:03:58 -0400

    isUnique

--- ArraysStringsHashTables/src/main/java/_01_is_unique/IsUnique.java
+++ ArraysStringsHashTables/src/main/java/_01_is_unique/IsUnique.java
@@ -1,5 +1,8 @@
 package _01_is_unique;
 
-public class IsUnique {
+// Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
+// isUnique("abcde") => true;
+// isUnique("abcded") => false;
 
+public class IsUnique {
 }
