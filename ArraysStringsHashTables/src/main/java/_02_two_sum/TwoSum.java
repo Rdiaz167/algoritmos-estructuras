0f85da160f5d599e35293b89806fe88d99112223
roberto <rd.navaro@gmail.com>
2024-08-27 20:24:54 -0400

    twosum

--- ArraysStringsHashTables/src/main/java/_02_two_sum/TwoSum.java
+++ ArraysStringsHashTables/src/main/java/_02_two_sum/TwoSum.java
@@ -1,5 +1,21 @@
 package _02_two_sum;
 
+
+// Dado un array de números enteros y un target, retorna los índices de dos
+// números para los que la suma de ambos sea igual al target.
+
+// Puedes asumir que hay solamente una solución.
+
+// Ejemplo 1:
+// Input: nums = [9,2,5,6], target = 7
+// Output: [1,2]
+// Explicación: nums[1] + nums[2] == 7, devolvemos [1, 2].
+
+// Ejemplo 2:
+// Input: nums = [9,2,5,6], target = 100
+// Output: null
+
+
 public class TwoSum {
 
 }
