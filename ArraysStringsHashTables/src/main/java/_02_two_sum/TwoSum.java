0dfe002871b875f9abf2ec833260b0fbc2a9e56b
roberto <rd.navaro@gmail.com>
2024-09-10 20:50:22 -0300

    twosumstest

--- ArraysStringsHashTables/src/main/java/_02_two_sum/TwoSum.java
+++ ArraysStringsHashTables/src/main/java/_02_two_sum/TwoSum.java
@@ -1,5 +1,8 @@
 package _02_two_sum;
 
+import java.util.Map;
+import java.util.HashMap;
+
 
 // Dado un array de números enteros y un target, retorna los índices de dos
 // números para los que la suma de ambos sea igual al target.
@@ -17,4 +20,18 @@ package _02_two_sum;
 
 
 public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        if(nums == null || nums.length < 2) return null;
+
+        Map<Integer, Integer> comMap = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(comMap.containsKey(nums[i])) return new int[]{i, comMap.get(nums[i])};
+
+            int complement = target - nums[i];
+            comMap.put(complement, i);
+        }
+        return null;
+    }
 }
