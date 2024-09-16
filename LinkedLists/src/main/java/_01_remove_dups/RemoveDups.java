f5a1e1bae28215e24f36331c48e7a31298d3aa69
roberto <rd.navaro@gmail.com>
2024-09-16 20:51:19 -0300

    removedups

--- LinkedLists/src/main/java/_01_remove_dups/RemoveDups.java
+++ LinkedLists/src/main/java/_01_remove_dups/RemoveDups.java
@@ -1,5 +1,22 @@
 package _01_remove_dups;
 
+import java.util.HashSet;
+
+import org.w3c.dom.Node;
+
 public class RemoveDups {
 
+    public void removeDups(Node head) {
+        HashSet<Integer> valuesFound = new HashSet<>();
+        valuesFound.add(head.value);
+
+        Node current = head;
+        while(current != null && current.next != null) {
+            if(!valuesFound.add(current.next.value)) {
+                current.next = current.next.next;
+            }
+            current = current.next;
+        }
+    }
+    
 }
