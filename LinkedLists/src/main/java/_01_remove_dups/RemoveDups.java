042e5b324de55d84c068c9f8dd52dfb686c2776c
roberto <rd.navaro@gmail.com>
2024-09-22 13:57:04 -0300

    removedupsproblem

--- LinkedLists/src/main/java/_01_remove_dups/RemoveDups.java
+++ LinkedLists/src/main/java/_01_remove_dups/RemoveDups.java
@@ -4,6 +4,11 @@ import java.util.HashSet;
 
 import _00_linkedlist.Node;
 
+// Escribe un algoritmo para eliminar los elementos duplicados en una Lista enlazada
+// Ejemplo:
+// Input: 1->2->2->3->4->1
+// Output: 1->2->3->4
+
 public class RemoveDups {
 
     public void removeDups(Node head) {
