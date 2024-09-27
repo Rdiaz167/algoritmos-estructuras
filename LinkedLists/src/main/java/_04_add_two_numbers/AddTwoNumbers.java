71f5543d8dccfa22b424fea9e0c1b4a4aa208a10
roberto <rd.navaro@gmail.com>
2024-09-26 23:15:03 -0300

    enunciadoaddtwonumbers

--- LinkedLists/src/main/java/_04_add_two_numbers/AddTwoNumbers.java
+++ LinkedLists/src/main/java/_04_add_two_numbers/AddTwoNumbers.java
@@ -1,5 +1,15 @@
 package _04_add_two_numbers;
 
+/*
+ * Escribe un algoritmo que realice la suma de dos listas que representan
+ * dos enteros positivos. Las listas están en posición invertida.
+ *
+ * Ejemplo:
+ *  Input: 1->2->4, 5->2->8
+ *  Output: 6->4->2->1
+ *  6421 + 825 = 7246 
+ */
+
 public class AddTwoNumbers {
 
 }
