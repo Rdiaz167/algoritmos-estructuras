9e1bfa61d8db0ea2b8f2f2d2743b354aeb2ead24
roberto <rd.navaro@gmail.com>
2024-10-06 15:04:04 -0300

    queuewhitstacks

--- StacksQueues/src/main/java/_01_queue_with_stacks/QueueWithStacks.java
+++ StacksQueues/src/main/java/_01_queue_with_stacks/QueueWithStacks.java
@@ -1,5 +1,8 @@
 package _01_queue_with_stacks;
 
+// * Como Stack usaremos la Interfaz Deque, que proporciona las operaciones de una cola doblemente terminada,
+// * permitiéndola usar como Pila o Cola (solo las operaciones de Pila están permitidas en este ejercicio).
+
 public class QueueWithStacks {
 
 }
