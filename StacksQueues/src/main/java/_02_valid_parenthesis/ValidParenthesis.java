537d64395d1128453995bc82c3146b27050373ae
roberto <rd.navaro@gmail.com>
2024-10-07 20:12:15 -0300

    validparenthesis

--- StacksQueues/src/main/java/_02_valid_parenthesis/ValidParenthesis.java
+++ StacksQueues/src/main/java/_02_valid_parenthesis/ValidParenthesis.java
@@ -1,5 +1,18 @@
 package _02_valid_parenthesis;
 
+/*
+ * Dado un String que solamente contiene los caracteres '(', ')', '{', '}', '[' y ']',
+ * implementa un algoritmo para determinar si es válido.
+ *
+ * Ejemplo 1:
+ *  Input: "([]){}"
+ *  Output: true
+ *
+ * Ejemplo 2:
+ *  Input: "({)}"
+ *  Output: false
+ */
+
 public class ValidParenthesis {
 
 }
