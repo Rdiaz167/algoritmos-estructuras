ecc9a5f2672f4f0c8f0b12d69f8e4057dc6f9386
roberto <rd.navaro@gmail.com>
2024-10-09 21:09:28 -0300

    stackmin

--- StacksQueues/src/main/java/_03_sort_stack/SortStack.java
+++ StacksQueues/src/main/java/_03_sort_stack/SortStack.java
@@ -11,5 +11,4 @@ package _03_sort_stack;
  */
 
 public class SortStack {
-
 }
--- StacksQueues/src/main/java/_04_stack_min/StackMin.java
+++ StacksQueues/src/main/java/_04_stack_min/StackMin.java
@@ -1,5 +1,10 @@
 package _04_stack_min;
 
+/*
+ * ¿Cómo diseñarías un Stack que además de las operaciones de push y pop también
+ * contase con una operación para obtener el mínimo?
+ */
+
 public class StackMin {
 
 }
