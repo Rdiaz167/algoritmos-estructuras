101b83d17fd07cb41ae7654029c500e13c12bcfc
roberto <rd.navaro@gmail.com>
2024-10-11 21:04:01 -0300

    addtwonumbers

--- LinkedLists/src/main/java/_04_add_two_numbers/AddTwoNumbers.java
+++ LinkedLists/src/main/java/_04_add_two_numbers/AddTwoNumbers.java
@@ -1,5 +1,7 @@
 package _04_add_two_numbers;
 
+import _00_linkedlist.Node;
+
 /*
  * Escribe un algoritmo que realice la suma de dos listas que representan
  * dos enteros positivos. Las listas están en posición invertida.
@@ -12,4 +14,33 @@ package _04_add_two_numbers;
 
 public class AddTwoNumbers {
 
+    public Node addTwoNumbers(Node list1, Node list2) {
+        Node result = new Node(-1);
+        Node current = result;
+        int carry = 0;
+
+        while(list1 != null || list2 != null) {
+            current.next = new Node(-1);
+            current = current.next;
+            int digit = carry;
+
+            if(list1 != null) {
+                digit += list1.value;
+                list1 = list1.next;
+            }
+            if(list2 != null) {
+                digit += list2.value;
+                list2 = list2.next;
+            }
+            carry = digit / 10;
+            digit = digit % 10;
+
+            current.value = digit;
+        }  
+        
+        if(carry > 0) {
+            current.next = new Node(carry);
+        }
+        return result.next;
+    }
 }
