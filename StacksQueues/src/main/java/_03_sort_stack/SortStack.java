3796b68e52024833efa43e7ad8d16c784c1b2ca7
roberto <rd.navaro@gmail.com>
2024-10-19 22:59:31 -0300

    sortstack

--- StacksQueues/src/main/java/_03_sort_stack/SortStack.java
+++ StacksQueues/src/main/java/_03_sort_stack/SortStack.java
@@ -1,5 +1,8 @@
 package _03_sort_stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
  * Ordena un Stack de forma que los elementos más pequeños queden en el tope del Stack.
  * Puedes usar un Stack adicional, pero no está permitido copiar los elementos a otra
@@ -11,5 +14,17 @@ package _03_sort_stack;
  */
 
 public class SortStack {
-    public
+    public Deque<Integer> sort(Deque<Integer> stack) {
+        Deque<Integer> sortedStack = new ArrayDeque<>();
+
+        while(!stack.isEmpty()) {
+            Integer element = stack.pop();
+            while((!sortedStack.isEmpty() && element > sortedStack.peek())) {
+                stack.push(sortedStack.pop());
+            }
+            sortedStack.push(element);
+        }
+        return sortedStack;
+    }
+    
 }
