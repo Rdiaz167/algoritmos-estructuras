9e6bb6f1fc38ff994b2109bf3294f15b5ea33a23
roberto <rd.navaro@gmail.com>
2024-10-21 22:15:25 -0300

    queuetest

--- StacksQueues/src/main/test/_01_queue_with_stacks/QueueWithStacksTest.java
+++ StacksQueues/src/main/test/_01_queue_with_stacks/QueueWithStacksTest.java
@@ -1,3 +1,21 @@
 public class QueueWithStacksTest {
-    
+
+    @Test
+    public void queueWithStacksTest() {
+      QueueWithStacks queueWithStacks = new QueueWithStacks();
+  
+      assertEquals(0, queueWithStacks.size());
+      queueWithStacks.add(1);
+      queueWithStacks.add(2);
+      queueWithStacks.add(3);
+      assertEquals(1, queueWithStacks.remove());
+      assertEquals(2, queueWithStacks.peek());
+      assertEquals(2, queueWithStacks.size());
+      queueWithStacks.add(4);
+      assertFalse(queueWithStacks.isEmpty());
+      assertEquals(2, queueWithStacks.remove());
+      assertEquals(3, queueWithStacks.remove());
+      assertEquals(4, queueWithStacks.remove());
+      assertTrue(queueWithStacks.isEmpty());
+    }
 }
